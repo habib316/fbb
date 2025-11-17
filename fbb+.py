edd2f4d2c6572ae0feb93cf166707f879973991d
habib316 <77967528+habib316@users.noreply.github.com>
2025-11-17 21:45:45 +0100

    Update fbb+.py

--- fbb+.py
+++ fbb+.py
@@ -8,7 +8,7 @@ from cadow import fbbrute
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
-def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prYellow(skk): print(\033[93m {}\033[00m" .format(skk)) 
 def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
 def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
